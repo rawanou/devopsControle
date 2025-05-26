8d30139219ba40d7615f4c6ad39b9fe88a6b0ef9
RAWANE <rawane18@outlook.fr>
2025-05-26 17:51:06 +0100

    4 commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero: React.FC = () => {
                 </div>
             </div>
 
-                        <p className="mt-4 text-foreground max-w-lg mx-auto">RAWANE khoumri</p>
+                        
 
             <div className="absolute left-0 right-0 bottom-0 backdrop-blur-[2px] h-40 bg-gradient-to-b from-transparent via-[rgba(233,238,255,0.5)] to-[rgba(202,208,230,0.5)]">
             </div>
