959b97359d70f2ec55d02c0c1b14c217ef033b1a
RAWANE <rawane18@outlook.fr>
2025-05-27 11:18:28 +0100

    another commit

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
-    heading: 'Smart, Secure, Simple Financial Management',
+    heading: 'simple, smart, secure Financial Management',
     subheading: 'From effortless budgeting to real-time investment insights, Finwise puts you in control of your money like never before',
     centerImageSrc: '/images/hero-mockup.webp',
 }
